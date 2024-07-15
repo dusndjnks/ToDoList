41a6385e279d6d19fb4e6e4bd6a58f50801f4d1e
dusndjnks <mhdaffan0000@gmail.com>
2024-07-15 14:47:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState,useEffect } from "react";
+import Todo from "./components/Todo";
+
+
+ const localData=()=>{
+  let list = localStorage.getItem("data")
+  if (list){
+    return JSON.parse(list)
+  }else{
+    return[]
+  }
+ }
 
 function App() {
+  const [todoList,setTodoList]=useState(localData())
+  const [newTask,setNewTask]=useState("")
+ 
+  // addTask
+  const addTask=(e)=>{
+    e.preventDefault()
+    if(!newTask){
+      alert("Enter a task")
+    }else{
+      let task={
+        id:todoList.length===0 ? 1 : todoList[todoList.length-1].id+1,
+        taskName:newTask,
+        completed:false
+      }
+      const newTodoList=[...todoList,task]
+      setTodoList(newTodoList)
+      setNewTask("")
+    }}
+    useEffect(()=>{
+      localStorage.setItem("data",JSON.stringify(todoList))
+    },[todoList])
+  
+
+    //deleteTask
+     const deleteTask=(id)=>{
+        const newTodoList = todoList.filter((item)=>{
+          return item.id !== id
+        })
+        setTodoList(newTodoList)
+     }
+
+     //updateTask
+     const updateTask=(id)=>{
+        let changeTask =todoList.find((item)=>{
+          return item.id === id
+        })
+        let newTodoList=todoList.filter((item)=>{
+          return item.id !== id
+        })
+        setNewTask(changeTask.taskName)
+        setTodoList(newTodoList)
+     }    
+
+    //  completedTask
+    const complete=(id)=>{
+      setTodoList(todoList.map((item)=>{
+        if(item.id === id){
+          return{...item,completed:true}
+        }else{
+          return item
+        }
+      }))
+    }
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-slate-300 h-screen  flex flex-col justify-center items-center gap-5 ">
+       <form action=""  className=" gap-6 flex ">
+       <input value={newTask} onChange={(e)=>{setNewTask((e).target.value)}} placeholder="Add To-Do-List" type="text" name="" id="" className="text-lg py-2 px-2 rounded-md"/>
+       <button className="bg-black text-white px-2 rounded-md text-lg font-bold"  onClick={addTask}>ADD</button>
+       </form>
+       <div>
+        
+        {todoList && todoList.map((item, index)=>{
+          return(
+        <Todo index={index} item={item} deleteTask={deleteTask} updateTask={updateTask} complete={complete} />
+        
+          )
+        })}
+        </div>
+       
     </div>
   );
 }
---
+++ src/components/Todo.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import DoneIcon from '@mui/icons-material/Done'
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+
+const Todo = ({index,item,deleteTask,updateTask,complete}) => {
+  return (
+    <div key={index} className="flex gap-3">
+    <div>
+    <h1 className={`list-decimal text-xl font-semibold w-[200px] ${item.completed ? "line-through text-green-600": "text-black"}`}>{index+1}. {item.taskName}</h1>
+    </div>
+    <div className="flex gap-2">
+    <DoneIcon className="text-green-600 cursor-pointer" onClick={()=>complete(item.id)}/>
+    <EditIcon className="text-blue-600 cursor-pointer " onClick={()=>updateTask(item.id)}/>
+    <DeleteIcon className="text-red-600 cursor-pointer" onClick={()=>deleteTask(item.id)}/>
+       
+    </div>
+  </div> 
+  )
+}
+
+export default Todo
